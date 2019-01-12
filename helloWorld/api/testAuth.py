c2747a56ed89023d0a5594dc55d22a8de37bf1e1
Rohit Vinnakota <rohitvinnakota@gmail.com>
2019-01-12 13:50:04 -0700

    test auth

--- helloWorld/api/testAuth.py
+++ helloWorld/api/testAuth.py
@@ -12,16 +12,15 @@ client = "41af0e72781d46a0a52fac7f3860d7f0"
 secret = "6b888f52bd224b9891fef6c35f178b5f"
 
 api_URL = "https://accounts.spotify.com/api/token"
-
 urlRequest = "https://api.spotify.com/v1/albums/04rz93AqGy9JduzV3K81Dh/tracks"
 def main():
+	grant_type = 'client_credentials'
+
+	body_params = {'grant_type' : grant_type}
+
 	# AUTH.
-	usrPass = client +':' + secret
-	b64Val = base64.b64encode(bytes(usrPass, 'utf-8'))
-	r=requests.post(api_URL, 
-                headers={"Authorization": "Basic %s" % b64Val},
-                data={"grant_type" : "client_credentials"})
-	print(r.json())
+	response=requests.post(api_URL, data=body_params, auth = (client, secret)) 
+	print(response.json())
 
 
 # usrPass = "userid:password"
