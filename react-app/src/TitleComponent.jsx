9b4366422b0a13a6c1d92db1be900aaf304bcb61
Rohit Vinnakota <rohitvinnakota@gmail.com>
2019-01-12 13:32:30 -0700

    added files

---
+++ react-app/src/TitleComponent.jsx
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react'
+
+
+class TitleComponent extends Component {
+    constructor() {
+        super()
+    }
+
+    makeTitleCallBasedOnSearch() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>The Lord of the Rings: Fellowship of the Ring</p>
+            </div>
+        )
+    }
+
+
+
+}
+
+
+
+export default TitleComponent
+
+
